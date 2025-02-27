6383dab88e5963e080c9db48d137b4eeae34c6bb
inawechsler <inaki.wechsler@gmail.com>
2025-02-27 00:04:50 -0300

    index correct espero

--- PortDev/obj/Debug/net8.0/PortDev.AssemblyInfo.cs
+++ PortDev/obj/Debug/net8.0/PortDev.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortDev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eddd54eac6973f6591798bde8e2dc733f14b804")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortDev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortDev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
